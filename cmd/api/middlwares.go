64ae79ea1b4ad807fe4ffcbe61e6e2dd93fdeceb
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2024-05-18 16:12:29 +0300

    Add recover panic

--- cmd/api/middlwares.go
+++ cmd/api/middlwares.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"sync"
@@ -8,6 +9,19 @@ import (
 
 	"golang.org/x/time/rate"
 )
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
 	type client struct {
